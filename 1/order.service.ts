68cca70d9ea3877ad5e36cf4a6e6961e00f19d44
n-ogata-cv-00010 <naoki.ogata@creava.co.jp>
2025-04-15 14:40:29 +0900

    👍 banana

--- 1/order.service.ts
+++ 1/order.service.ts
@@ -7,5 +7,7 @@ export function getCakeOrderMethod(type: CakeType) {
     console.log('【Anti Pattern】🧀チーズケーキが注文されました')
   } else if (type === 'apple') {
     console.log('【Anti Pattern】🍎アップルケーキが注文されました')
+  } else if (type === 'banana') {
+    console.log('【Anti Pattern】🍌バナナケーキが注文されました')
   }
 }
--- 1/order.type.ts
+++ 1/order.type.ts
@@ -1,1 +1,1 @@
-export type CakeType = 'chocolate' | 'cheese' | 'fruit' | 'apple';
+export type CakeType = 'chocolate' | 'cheese' | 'fruit' | 'apple' | 'banana';
